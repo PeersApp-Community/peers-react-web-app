66cfb2a800f68c200ad09fce85a53ec9b24aa0cb
roji-tech <rojitech9@gmail.com>
2022-07-02 22:58:43 +0100

    added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,15 +9,15 @@ const About = () => {
     <Section id="about" mt="26%">
       <Grid>
         <h3>About Us</h3>
-
         {/* <AboutFlex>
           <img src={Group1} alt="" /> */}
-
         <AboutFlex smMt="100px">
           <img src={Group1} alt="" />
 
           <Text>
-            <h4>Learn</h4>
+            <h4>
+              Learn | Network | Redefine Your <br /> School Experience
+            </h4>
             <p>
               The app consists of an Inbuilt library. It also helps with
               schedule planning and it stores all data on the server so you
@@ -28,10 +28,9 @@ const About = () => {
             </Button>
           </Text>
         </AboutFlex>
-
-        <AboutFlex smMt="250px" reverse="column-reverse">
+        {/* <AboutFlex smMt="250px" reverse="column-reverse">
           <Text>
-            <h4>Network</h4>
+            <h4></h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> A
               iure pariatur placeat odio cum ut est accusamus totam.{" "}
@@ -46,14 +45,11 @@ const About = () => {
         {/* 
         <AboutFlex>
           <img src={Group6} alt="" /> */}
-
         <AboutFlex smMt="300px">
           <img src={Group6} alt="" />
 
           <Text>
-            <h4>
-              Redefine Your <br /> School Experience
-            </h4>
+            <h4></h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> A
               iure pariatur placeat odio cum ut est accusamus totam.{" "}
@@ -62,7 +58,8 @@ const About = () => {
               Learn More
             </Button>
           </Text>
-        </AboutFlex>
+        </AboutFlex>{" "}
+        */}
       </Grid>
     </Section>
   );
